c5521a8167037867430ca0f3e462993f299d1eb1
MICHAEL COYLE <MACCOYLE@GMAIL.com>
2022-03-15 04:54:13 -0400

    updated restfuls

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,37 @@
 class Movie < ApplicationRecord
+  def index
+    movies = Movie.all
+    render json: movies.as_json
+  end
+
+  def create
+    movie = Movie.create(
+      title: params[:title],
+      year: params[:year],
+      plot: params[:plot]
+    )
+    render json: movie.as_json
+  end
+
+  def show
+    movie = Movie.find_by(id: params[:id])
+    render json: movie.as_json
+  end
+
+  def update
+    movie = Movie.find_by(id: params[:id])
+
+    movie.title = params[:title] || movie.title
+    movie.year = params[:year] || movie.year
+    movie.plot = params[:plot] || movie.plot
+
+    movie.save
+    render json: {message: "updated"}
+  end
+
+  def delete
+    movie = Movie.find_by(id: params[:id])
+    movie.destroy
+    render json: {message: "destroyed"}
+  end
 end
